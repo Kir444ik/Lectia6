7b9b536a6ab71cd78dda7d3cf7e8403bc7850a2c
Kir444ik <144593324+Kir444ik@users.noreply.github.com>
2023-10-17 13:47:38 +0300

    End HomeWork

---
+++ Ex41/Program.cs
@@ -0,0 +1,22 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+Console.WriteLine(" What you want enter number ?");
+int kol = Convert.ToInt32(Console.ReadLine());
+int  integers = 0;
+int[] array = FillingMassiv(kol);
+
+int[] FillingMassiv( int size ){
+    int[] arr = new int[size]; 
+    for( int i = 0 ; i < size ; i++){
+        arr[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    return arr;
+}
+Console.WriteLine("Your massiv");
+for ( int i = 0 ; i < kol ; i++)
+{   
+    if (array[i] > 0 ) integers+=1;
+    Console.Write($" {array[i]} " );
+}
+Console.Write($" ----> {integers} ");
---
+++ Ex43/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных 
+//уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+Console.WriteLine("Enter b1");
+double  b1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Enter k1");
+double  k1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Enter b2");
+double  b2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Enter k2");
+double k2 = Convert.ToInt32(Console.ReadLine());
+
+double x = Function(k1,b1,k2,b2);
+
+double Function(double K1,double B1,double K2,double B2){
+    double x = K1 - K2; 
+    double b = B2 - B1;
+    return b / x;
+}
+double y = k1 * x + b1;
+Console.WriteLine($" b1 = {b1} , k1 = {k1} , b2 = {b2} , k2 = {k2} -----> ({x} , {y}) ");
